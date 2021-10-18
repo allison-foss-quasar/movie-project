525519df19a9176e07f3e6447b2397f81049d95d
Robert Allison <robert.r.allison4@gmail.com>
2021-10-18 14:40:38 -0500

    have to commit to pull

--- movie_project.js
+++ movie_project.js
@@ -17,10 +17,12 @@ fetch('https://colossal-apricot-titanosaurus.glitch.me/movies')
         		`<h4>Genre: ${movie.genre} || Director: ${movie.director}</h4>${movie.year}` +
         		`<img src="${movie.poster}" style="width: 25em">` +
         		`<h4>Rating: ${movie.rating}</h4><h6>Description: ${movie.plot}</h6>` +
-        		'<div class="inline-flex m-2"><button type="button" class="modify">Modify</button><button type="button" class="delete">Delete</button></div>' +
+        		'<div class="inline-flex m-2"><button type="button" class="modify">Modify</button><button type="button" class="delete" data-id="${movie.id}">Delete</button></div>' +
 				'</div>'
 			});
 			document.getElementById('movies').innerHTML = html;
+			//add click listener
+			
         }) //Where we get the array of movies
     }).catch(function(err){console.log(err)});
 
